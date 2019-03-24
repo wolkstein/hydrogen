817fe5c54912d0a4eac989af448fcc4fa7ace3cf
wolkstein <m.wolkstein@gmx.de>
2019-03-24 15:54:40 +0100

    add midi pad arduino files

---
+++ Rpi-Midi-Pad/Rpi-Midi-Pad.ino
@@ -0,0 +1,120 @@
+/*
+ * Rpi-Pad Midi implementation 
+ * 
+ * drum Pad 1 - 8 Midi Note 
+ * Rotary 1 - 2 Midi CC
+ * Switch 1 - 2 Midi CC
+ * 
+ */
+
+
+// defines for setting and clearing register bits
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+// settings
+#define CONFIG_VERSION "R_2"
+
+struct StoreSettingsStruct {
+  // The variables of your settings
+  int midichannel;
+  int piezoSensing[8];
+  int padRetriggerWait;
+  int controlerloockup[8];
+  int padthreshold[8];
+    
+  // This is for mere detection if they are your settings
+  char version_of_program[4]; // it is the last variable of the struct
+  // so when settings are saved, they will only be validated if
+  // they are stored completely.
+} settings = {
+  // The default settings
+  1,                           // midichannel
+  {251,252,253,254,255,256,257,258},   // piezoSensing
+  69,                          // padRetriggerWait
+  {36,37,38,39,40,41,42,43},   // controlerloockup[8]
+  {21,22,23,24,25,26,27,28},   // padthreshold[8];
+  CONFIG_VERSION
+};
+
+int cVal[8] = {0,0,0,0,0,0,0,0};
+
+int hoester_wert[8] = {0,0,0,0,0,0,0,0};
+
+int padSamplingState[8] = {0,0,0,0,0,0,0,0}; // 0 = note off,2 = sampling, 1 = wait for retrigger
+
+unsigned long noteOnTimecVal[8] = {0,0,0,0,0,0,0,0};
+
+int sampleLenghtInMSeconds = 3; // we sample 3ms to catch highest velocity
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(11, OUTPUT);
+  Serial.begin(9600); 
+  sbi(ADCSRA,ADPS2) ;
+  cbi(ADCSRA,ADPS1) ;
+  cbi(ADCSRA,ADPS0) ;
+
+  //EEPROM
+  loadConfig();
+  // ###### MIDI Receiving Messages
+  initMidiReceiving();
+}
+
+
+void loop() {
+  usbMIDI.read();
+  unsigned long thisLooptime = millis();
+  //Serial.printf("loop %i\n",thisLooptime);
+  //Serial.printf("padstate %i\n",padSamplingState[0]);
+
+  for(int i = 0;i<8;i++){
+    cVal[i] = analogRead(i);
+    //if(i<7) cVal[i] = 0; // nicht angeschlossene pads floaten rum
+  }
+
+  //Serial.printf("pad 7 %i\n",cVal[7]);
+  // find firdt trigger moment from pad
+  for(int i = 0;i<8;i++){
+    if(cVal[i] > settings.padthreshold[i] && padSamplingState[i] < 1){
+      //Serial.printf("%i on pad %i, start Sampling\n",cVal[i],i);
+      noteOnTimecVal[i] = thisLooptime;
+      padSamplingState[i] = 2;     
+    }
+  }
+  
+  // sample active pads
+  for(int i = 0;i<8;i++){
+    if( padSamplingState[i] == 2 ){
+      if( cVal[i] > hoester_wert[i] ) hoester_wert[i] = cVal[i];
+      //Serial.printf("Do Sampling Sample pad %i, h_v = %i, s_v = %i \n",i,hoester_wert[i],cVal[i]);
+      // checke ob sampletime am pad erreicht ist
+      if(millis() >= (noteOnTimecVal[i] + sampleLenghtInMSeconds)){
+        padSamplingState[i] = 1;
+        //Serial.printf("End Sampling on Sample pad %i state 1 hw = %i\n",i,hoester_wert[i]);
+        // nun können wir midi feuern
+        if(hoester_wert[i] > settings.piezoSensing[i]) hoester_wert[i] = settings.piezoSensing[i];
+        usbMIDI.sendNoteOn( settings.controlerloockup[i], map(hoester_wert[i], settings.padthreshold[i], settings.piezoSensing[i], 1, 127), settings.midichannel);
+        hoester_wert[i] = 0;
+        digitalWrite(11,HIGH);
+        
+      }
+    }
+
+  }
+
+  //warte auf pad retrigger
+   for(int i = 0;i<8;i++){
+     if(padSamplingState[i] == 1 && thisLooptime >= noteOnTimecVal[i] + sampleLenghtInMSeconds + settings.padRetriggerWait){
+      padSamplingState[i] = 0;
+      digitalWrite(11,LOW);
+      //Serial.printf("Retrigger end pad %i state 0\n",0);
+      
+    }    
+   } 
+}
---
+++ Rpi-Midi-Pad/midi.ino
@@ -0,0 +1,128 @@
+char pass[3] = {0,119};
+bool passed[3] = {false,false};
+unsigned long messagecounter = 0;
+unsigned long oldmessagecounter = 0;
+
+
+char n_pass[3] = {1,118};
+bool n_passed[3] = {false,false};
+unsigned long n_messagecounter = 0;
+unsigned long n_oldmessagecounter = 0;
+
+int info_for_next_settings_value = 1000;
+
+//################# init
+void initMidiReceiving(){
+  usbMIDI.setHandleNoteOn(OnNoteOn);
+  usbMIDI.setHandleNoteOff(OnNoteOff);
+  usbMIDI.setHandleControlChange(OnControlChange);
+  usbMIDI.setHandlePitchChange(OnPitchBend);
+} 
+
+
+//################ implementation
+
+void OnControlChange(byte channel, byte control, byte value){
+  //Serial.printf("Receive Midi CC: %d | VAL: %d | CHANNEL: %d \n",control, value, channel );
+}
+
+void OnNoteOn(byte channel, byte note, byte velocity){
+  //Serial.printf("Receive Midi Note On, NOTE: %d | VEL: %d\n",note, velocity );
+
+  
+}
+  
+void OnNoteOff(byte channel, byte note, byte velocity){
+  //Serial.printf("Receive Midi Note Off, NOTE: %d | VEL: %d\n",note, velocity );
+
+  // midi settings question
+  messagecounter++;
+  n_messagecounter++;
+  
+  if( note == pass[0] && velocity == 0){
+    oldmessagecounter = messagecounter;
+    passed[0] = true;
+  }
+  if(passed[0] && note == pass[1] && velocity ==0 && oldmessagecounter + 1 == messagecounter){
+    passed[1] = true;
+  }
+
+  if( passed[0] && passed[1]){
+    //Serial.println("send midi settings");
+    for(int i = 0 ; i<8;i++){
+      usbMIDI.sendPitchBend( settings.padthreshold[i], settings.midichannel);
+      usbMIDI.sendPitchBend( settings.piezoSensing[i], settings.midichannel);
+      usbMIDI.sendPitchBend( settings.controlerloockup[i], settings.midichannel);
+    }
+
+    //midichannel
+    usbMIDI.sendPitchBend( settings.midichannel, settings.midichannel);
+    // padretriggerwait
+    usbMIDI.sendPitchBend( settings.padRetriggerWait, settings.midichannel);
+    // endmessage
+    usbMIDI.sendPitchBend(8190, settings.midichannel);// endmessage 8190    
+    passed[0] = false;
+    passed[1] = false;
+  }
+
+  // midi new value
+  if( note == n_pass[0] && velocity == 0){
+    n_oldmessagecounter = n_messagecounter;
+    n_passed[0] = true;
+  }
+  if(n_passed[0] && note == n_pass[1] && n_oldmessagecounter + 1 == n_messagecounter){
+    n_passed[1] = true;
+    info_for_next_settings_value = velocity;
+  }
+
+
+}
+
+void OnPitchBend(byte channel, int pitch){
+  if( n_passed[0] && n_passed[1]){
+    //Serial.printf("pad position %d, set value to %d\n",info_for_next_settings_value,pitch);  
+
+    //pad 1
+    if(info_for_next_settings_value == 1) settings.padthreshold[0] = pitch;
+    if(info_for_next_settings_value == 2) settings.piezoSensing[0] = pitch;
+    if(info_for_next_settings_value == 3) settings.controlerloockup[0] = pitch;
+    //pad 2
+    if(info_for_next_settings_value == 4) settings.padthreshold[1] = pitch;
+    if(info_for_next_settings_value == 5) settings.piezoSensing[1] = pitch;
+    if(info_for_next_settings_value == 6) settings.controlerloockup[1] = pitch;
+     //pad 3
+    if(info_for_next_settings_value == 7) settings.padthreshold[2] = pitch;
+    if(info_for_next_settings_value == 8) settings.piezoSensing[2] = pitch;
+    if(info_for_next_settings_value == 9) settings.controlerloockup[2] = pitch;
+    //pad 4
+    if(info_for_next_settings_value == 10) settings.padthreshold[3] = pitch;
+    if(info_for_next_settings_value == 11) settings.piezoSensing[3] = pitch;
+    if(info_for_next_settings_value == 12) settings.controlerloockup[3] = pitch;
+
+    
+    //pad 5
+    if(info_for_next_settings_value == 13) settings.padthreshold[4] = pitch;
+    if(info_for_next_settings_value == 14) settings.piezoSensing[4] = pitch;
+    if(info_for_next_settings_value == 15) settings.controlerloockup[4] = pitch;
+    //pad 6
+    if(info_for_next_settings_value == 16) settings.padthreshold[5] = pitch;
+    if(info_for_next_settings_value == 17) settings.piezoSensing[5] = pitch;
+    if(info_for_next_settings_value == 18) settings.controlerloockup[5] = pitch;
+     //pad 7
+    if(info_for_next_settings_value == 19) settings.padthreshold[6] = pitch;
+    if(info_for_next_settings_value == 20) settings.piezoSensing[6] = pitch;
+    if(info_for_next_settings_value == 21) settings.controlerloockup[6] = pitch;
+    //pad 8
+    if(info_for_next_settings_value == 22) settings.padthreshold[7] = pitch;
+    if(info_for_next_settings_value == 23) settings.piezoSensing[7] = pitch;
+    if(info_for_next_settings_value == 24) settings.controlerloockup[7] = pitch;
+
+    if(info_for_next_settings_value == 25) settings.padRetriggerWait = pitch;
+
+    if(info_for_next_settings_value == 26) saveConfig();
+    
+    n_passed[0] = false;
+    n_passed[1] = false;
+    info_for_next_settings_value = 1000;
+  }   
+}
